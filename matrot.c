16b96a50275cc4ef3c4fe250b1084b825aea6500
Ashma-Garg <ashmagarg77u@gmail.com>
2019-08-03 10:25:51 +0530

    Rotation of array by 1

---
+++ matrot.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+void rotatematrix(int m, int n, int mat[m][n]) 
+{ 
+    int row = 0, col = 0; 
+    int prev, curr; 
+
+    int R=m,C=n;
+    while (row < m && col < n) 
+    { 
+  
+        if (row + 1 == m || col + 1 == n) 
+            break; 
+  
+        
+ 	prev = mat[row + 1][col]; 
+  
+        
+        for (int i = col; i < n; i++) 
+        { 
+            curr = mat[row][i]; 
+            mat[row][i] = prev; 
+            prev = curr; 
+        } 
+        row++; 
+  
+        
+        for (int i = row; i < m; i++) 
+        { 
+            curr = mat[i][n-1]; 
+            mat[i][n-1] = prev; 
+            prev = curr; 
+        } 
+        n--; 
+  
+        
+        if (row < m) 
+        { 
+            for (int i = n-1; i >= col; i--) 
+            { 
+                curr = mat[m-1][i]; 
+                mat[m-1][i] = prev; 
+                prev = curr; 
+            } 
+        } 
+        m--; 
+  
+        
+        if (col < n) 
+        { 
+            for (int i = m-1; i >= row; i--) 
+            { 
+                curr = mat[i][col]; 
+                mat[i][col] = prev; 
+                prev = curr; 
+            } 
+        } 
+        col++; 
+    } 
+   
+    for (int i=0; i<R; i++) 
+    { 
+        for (int j=0; j<C; j++) 
+          printf("%d ",mat[i][j]); 
+       printf("\n"); 
+    } 
+} 
+int main() 
+{ 
+	int n;
+	scanf("%d",&n);
+    int arr[n][n]; 
+  for(int i=0;i<n;i++)
+  {
+  
+	  for(int j=0;j<n;j++)
+	  {
+		  scanf("%d",&arr[i][j]);
+	  }
+  } 
+     rotatematrix(n, n, arr); 
+    return 0; 
+} 
